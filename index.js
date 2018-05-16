c2e659153bca78e7292b15771dd1837576281818
Edsel Caprice <caprice@mail.usf.edu>
2018-05-16 02:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase());
 }
 
-function sayHiToGrandma (string) {
+function sayHiToGrandma(string) {
   if string.toLowerCase() === string then return "I can\'t hear you!"
   else if string === string.toUpperCase()
   then return "YES INDEED"
